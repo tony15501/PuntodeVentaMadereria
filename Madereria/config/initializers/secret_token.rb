297c14b2e205fab9bb05795f7e00bc677143d355
Uriel7 <j.urielsz@gmail.com>
2014-05-20 19:53:30 -0500

    punto de venta

---
+++ Madereria/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madereria::Application.config.secret_key_base = 'de39fbd4534cd134ff1fb46232a40d82126c45717ab70a27ac282c739af8acd7fb9a8de4d0b357e6f9ed98e61a49ac48a061480ab8a8b4b73b4e0d929ca6f639'
